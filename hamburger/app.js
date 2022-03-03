7f5d2a87b9b47f65910e395fe4c8908a8e003146
Artem <jaksan37@gmail.com>
2022-03-04 00:56:41 +0300

    fix some

--- hamburger/app.js
+++ hamburger/app.js
@@ -1,19 +1,23 @@
 class Hamburger {
-    constructor(size, stuff, options, sizeCalc, stuffCalc, optionsCalc) {
+    constructor(size, stuff, options, sizeCalc, stuffCalc, optionsCalc, result, resultCalc) {
         this.size = size;
         this.sizeCalc = sizeCalc;
         this.stuffCalc = stuffCalc;
         this.optionsCalc = optionsCalc;
         this.stuff = stuff;
         this.options = options;
+        this.result = result;
+        this.resultCalc = resultCalc;
         this.getSize();
         this.getStuff();
         this.getOptions();
-        this.list = [];
+        this.showAll();
     }
 
     getSize() {
         document.querySelector('.burger__list').addEventListener('click', (event) => {
+            document.querySelectorAll('.btn').forEach(el => el.classList.remove('active'));
+            event.target.classList.add('active');
             if (event.target.tagName !== 'BUTTON') {
                 return;
             }
@@ -32,36 +36,55 @@ class Hamburger {
     }
 
     getOptions() {
-        let values = [];
-        const arr = document.querySelectorAll(`.input`);
+        this.result = [];
+        this.resultCalc = [];
+        const arr = [...document.querySelectorAll('input[name="topings"]')];
         arr.forEach(el => {
-            el.addEventListener('click', (event) => {
-
-                let check = document.querySelectorAll('input[name="topings"]:checked');
-                check.forEach(el => {
-                    values.push(event.target.dataset["price"]);
-                })
+            el.addEventListener('change', (event) => {
+                if (event.target.checked === true) {
+                    this.options = el.dataset['price'];
+                    this.optionsCalc = el.dataset['calories']
+                    this.result.push(this.options);
+                    this.resultCalc.push(this.optionsCalc)
+                } else if (event.target.checked === false) {
+                    if (this.result.length == 1) {
+                        this.result = [];
+                        this.resultCalc = [];
+                    } else {
+                        this.result = [document.querySelector(`input[name=topings]:checked`).dataset.price];
+                        this.resultCalc = [document.querySelector(`input[name=topings]:checked`).dataset.calories];
+                    }
+                }
             })
-
         })
-        console.log(values);
+
+        return this.result, this.resultCalc;
     }
 
+    getSumPrice(size, sizeCalc, stuff, stuffCalc, result) {
+        let price = 0;
+        this.result.forEach(el => price += Number(el))
+        const totalPrice = +this.size + +this.stuff + price;
 
-    // inputs.forEach(el => {
-    //     el.addEventListener('click', () => {
-    //         container.textContent = '';
-    //         let input_checkeds = document.querySelectorAll('input:checked');
+        return totalPrice;
+    }
 
-    //         input_checkeds.forEach(el_checked => {
-    //             container.insertAdjacentHTML('beforeend', el_checked.value);
-    //         });
-    //     });
-    // });
+    getSumCalc(size, sizeCalc, stuff, stuffCalc, price, resultCalc) {
+        let calc = 0;
 
-    // this.options = event.target.dataset['price'];
-    // this.optionsCalc = event.target.dataset['calories'];
+        this.result.forEach(el => calc += Number(el))
 
+        const totalCalc = +this.sizeCalc + +this.stuffCalc + calc;
+
+        return totalCalc;
+    }
+
+    showAll() {
+        document.querySelector('.total-btn').addEventListener('click', () => {
+            document.querySelector('.totalPrice-span').textContent = this.getSumPrice();
+            document.querySelector('.totalCalc-span').textContent = this.getSumCalc();
+        })
+    }
 }
 
 
